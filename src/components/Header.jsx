c4ec844b3c2c25b18bf9f90e8e3c2da33d5dc367
Misato Watanabe <watamisa080513@gmail.com>
2023-12-17 01:59:12 +0900

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../style.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-text">Let's チューニング体験</div>
+      <div className="header-text">Let's チューニング</div>
       <div className="description">
         ドラッグしながら上下に引っ張ると音程を上げ下げすることができます
       </div>
